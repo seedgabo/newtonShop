db80f745aba2357301e54501c3419a38ad63d916
gabriel <seedgabo@gmail.com>
2018-10-20 22:51:46 -0500

    f

--- src/App.vue
+++ src/App.vue
@@ -107,7 +107,7 @@ export default {
 	opacity: 0;
 }
 .v-content {
-	background: url(static/background.jpg);
+	background: url(../../static/background.jpg);
 	background-size: contain;
 	background-attachment: fixed;
 }
